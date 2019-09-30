9720d6f848196284e9b62cd3536e088c5a215d51
Christian <christ.biz@gmail.com>
2019-09-30 15:58:21 +0700

    upd pom

---
+++ src/main/java/com/demo/MainSpringBoot.java
@@ -0,0 +1,24 @@
+package com.demo.time;
+
+import java.util.TimeZone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+@SpringBootApplication
+@EnableAutoConfiguration
+@ComponentScan(basePackages = { "com.demo.dao", "com.demo.service", "com.demo.controller"})
+@EnableTransactionManagement
+@EntityScan(basePackages = "com.demo.model")
+public class MainSpringBoot extends SpringBootServletInitializer{
+
+	public static void main(String[] args) {
+//		TimeZone.setDefault(TimeZone.getTimeZone("ASIA/JAKARTA"));
+		SpringApplication.run(MainSpringBoot.class, args);
+	}
+}
